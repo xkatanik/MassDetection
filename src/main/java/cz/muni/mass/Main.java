82059d388770d3769006dd38c2b98fe8ef03c23d
xkatanik <k.katanik@gmail.com>
2018-03-19 12:27:00 +0100

    Added options, methods.

--- src/main/java/cz/muni/mass/Main.java
+++ src/main/java/cz/muni/mass/Main.java
@@ -16,99 +16,95 @@ import net.sf.mzmine.parameters.parametertypes.selectors.ScanSelection;
 import net.sf.mzmine.project.impl.MZmineProjectImpl;
 import net.sf.mzmine.project.impl.ProjectManagerImpl;
 import net.sf.mzmine.project.impl.RawDataFileImpl;
-import org.xml.sax.SAXException;
-import javax.xml.parsers.ParserConfigurationException;
+import org.apache.commons.cli.*;
+import org.apache.commons.lang.NullArgumentException;
+
+
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Field;
 
-
-
 /**
+ * Mass detection module.
+ *
  * @author Kristian Katanik
  */
 public class Main {
 
-    public static void main(String[] args) throws ParserConfigurationException, IOException, SAXException, NoSuchFieldException, IllegalAccessException {
 
-        Integer i = 0;
-        String inputFileName = null;
-        String outputFileName = null;
-        Double noiseLevel = null;
+    public static void main(String[] args) throws IOException, NoSuchFieldException, IllegalAccessException {
+
+        String inputFileName;
+        String outputFileName;
+        Double noiseLevel;
         Integer scaleLevel = 2;
         Double windowSize = 0.1;
-        Boolean isCentroided = false;
-        while(i < args.length){
-            switch (args[i]){
-                case "-inputFile":
-                    inputFileName = args[i+1];
-                    i += 2;
-                    break;
-                case "-outputFile":
-                    outputFileName = args[i+1];
-                    i += 2;
-                    break;
-                case "-centroid":
-                    isCentroided = true;
-                    i++;
-                    break;
-                case "-wavelet":
-                    i++;
-                    break;
-                case "-noiseLevel":
-                    try {
-                        noiseLevel = Double.parseDouble(args[i+1]);
-                    } catch (Exception e){
-                        System.err.println("Missing or wrong format of -noiseLevel parameter.");
-                        return;
-                    }
-                    i += 2;
-                    break;
-                case "-scaleLevel":
-                    try {
-                        scaleLevel = Integer.parseInt(args[i+1]);
-                    } catch (Exception e){
-                        System.err.println("Missing or wrong format of -scaleLevel parameter.");
-                        return;
-                    }
-                    i += 2;
-                    break;
-                case "-windowSize":
-                    try {
-                        windowSize = Double.parseDouble(args[i+1]);
-                        windowSize = windowSize/100;
-                    } catch (Exception e){
-                        System.err.println("Missing or wrong format of -windowSize parameter.");
-                        return;
-                    }
-                    i += 2;
-                    break;
-                case "-help":
-                    System.out.println("Mass detection module.\n" +
-                            "This module detects individual ions in each scan and builds a mass list for each scan.\n"+
-                            "\n" +
-                            "Required parameters:\n" +
-                            "\t-inputFile | Name or path of input raw data file\n" +
-                            "\t-outputFile | Name or path of output file. File name must end with .CDF\n" +
-                            "\n" +
-                            "Optional parameters:\n" +
-                            "\t -centroid or -wavelet | Algorithm to use for mass detection and its parameters.\n" +
-                            "\t\t[default -wavelet]\n" +
-                            "\n" +
-                            "\tRequired parameter for -centroid:\n" +
-                            "\t\t-noiseLevel | Intensities less than this value are interpreted as noise.\n" +
-                            "\n" +
-                            "\tOptional parameters for -wavelet:\n" +
-                            "\t\t-noiseLevel | Intensities less than this value are interpreted as noise.\n"+ "" +
-                            "\t\t\t[default 1.0E2]\n" +
-                            "\t\t-scaleLevel | Number of wavelet'scale (coeficients) to use in m/z peak detection.\n" +
-                            "\t\t\t[default 2]\n" +
-                            "\t\t-windowSize | Size in % of wavelet window to apply in m/z peak detection.\n" +
-                            "\t\t\t[default 10.0]\n");
+        Boolean isCentroid = false;
+
+        Options options = setOptions();
+
+        if (args.length == 0) {
+            HelpFormatter helpFormatter = new HelpFormatter();
+            helpFormatter.setOptionComparator(null);
+            helpFormatter.printHelp("MassDetection module help.", options);
+            return;
+        }
+
+
+        CommandLine commandLine;
+        try {
+            commandLine = new DefaultParser().parse(options, args);
+        } catch (ParseException e) {
+            for (String arg : args) {
+                if (arg.equals("-h") || arg.equals("--help")) {
+                    HelpFormatter helpFormatter = new HelpFormatter();
+                    helpFormatter.setOptionComparator(null);
+                    helpFormatter.printHelp("MassDetection module help.", options);
                     return;
-                default:
-                    i++;
-                    break;
+                }
+            }
+            System.err.println("Some of the required parameters or their arguments are missing. Use -h or --help for help.");
+            return;
+        }
+
+
+        inputFileName = commandLine.getOptionValue("i");
+        outputFileName = commandLine.getOptionValue("o");
+
+        if (commandLine.hasOption("c")) {
+            isCentroid = true;
+        }
+
+        if (isCentroid) {
+            try {
+                noiseLevel = Double.parseDouble(commandLine.getOptionValue("nl"));
+            } catch (NumberFormatException e) {
+                System.err.println("Wrong format of noiseLevel value. Value has to be number in double format.");
+                return;
+            }
+        } else {
+            try {
+                noiseLevel = Double.parseDouble(commandLine.getOptionValue("nl"));
+            } catch (NumberFormatException e) {
+                System.err.println("Wrong format of noiseLevel value. Value has to be number in double format.");
+                return;
+            }
+            if (commandLine.hasOption("sc")) {
+                try {
+                    scaleLevel = Integer.parseInt(commandLine.getOptionValue("sl"));
+                } catch (NumberFormatException e) {
+                    System.err.println("Wrong format of scaleLevel value. Value has to be number in integer format.");
+                    return;
+                }
+            }
+            if (commandLine.hasOption("ws")) {
+                try {
+                    windowSize = Double.parseDouble(commandLine.getOptionValue("ws"));
+                    windowSize = windowSize / 100;
+                } catch (NumberFormatException e) {
+                    System.err.println("Wrong format of windowSize value. Value has to be number in double format.");
+                    return;
+                }
 
             }
         }
@@ -117,21 +113,20 @@ public class Main {
         File inputFile;
         try {
             inputFile = new File(inputFileName);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Unable to load input file.");
-            return;
-        }
-        File outputFile;
-        try {
-            outputFile = new File(outputFileName);
-        } catch(Exception e){
-            e.printStackTrace();
-            System.out.println("Unable to create/load output file.");
+        } catch (NullArgumentException e) {
+            System.err.println("Unable to load input file.");
             return;
         }
 
-        if(!inputFile.exists() || inputFile.isDirectory()){
+        File outputFile;
+        try {
+            outputFile = new File(outputFileName);
+        } catch (NullArgumentException e) {
+            System.err.println("Unable to create/load output file.");
+            return;
+        }
+
+        if (!inputFile.exists() || inputFile.isDirectory()) {
             System.err.println("Unable to load input/output file.");
             return;
         }
@@ -155,27 +150,44 @@ public class Main {
         projectManagerField.set(null, projectManager);
         projectManager.setCurrentProject(mZmineProject);
 
+        MassDetectionParameters parameters = setParameters(outputFile, isCentroid, noiseLevel, scaleLevel, windowSize);
+
+        MassDetectionTask massDetectionTask = new MassDetectionTask(rawDataFile, parameters);
+        massDetectionTask.run();
+
+
+    }
+
+    private static Options setOptions() {
+        Options options = new Options();
+        options.addOption(Option.builder("i").required().hasArg().longOpt("inputFile").desc("[required] Name or path of input raw data file. Type .mzXML").build());
+        options.addOption(Option.builder("o").required().hasArg().longOpt("outputFile").desc("[required] Name or path of output file. File name must end with .CDF").build());
+        options.addOption(Option.builder("c").required(false).longOpt("centroid").desc("Centroid mass detector. Algorithm to use for mass detection and its parameters.").build());
+        options.addOption(Option.builder("w").required(false).longOpt("wavelet").desc("Wavelet mass detector. Algorithm to use for mass detection and its parameters. [default]").build());
+        options.addOption(Option.builder("nl").required().hasArg().longOpt("noiseLevel").desc("[required] Intensities less than this value are interpreted as noise.").build());
+        options.addOption(Option.builder("sl").required(false).hasArg().longOpt("scaleLevel").desc("Parameter used by Wavelet mass detector. Number of wavelet'scale (coeficients) to use in m/z peak detection. [default 2]").build());
+        options.addOption(Option.builder("ws").required(false).hasArg().longOpt("windowSize").desc("Parameter used by Wavelet mass detector. Size in % of wavelet window to apply in m/z peak detection.[default 10% = 0.1]").build());
+        options.addOption(Option.builder("h").required(false).longOpt("help").build());
+
+        return options;
+
+    }
+
+    private static MassDetectionParameters setParameters(File outputFile, Boolean isCentroided, Double noiseLevel, Integer scaleLevel, Double windowSize) {
 
         MassDetectionParameters parameters = new MassDetectionParameters();
         MassDetector[] massDetectors = MassDetectionParameters.massDetectors;
-        if(isCentroided){
+        if (isCentroided) {
             CentroidMassDetectorParameters centroidMassDetectorParameters = new CentroidMassDetectorParameters();
-            if(noiseLevel == null){
-                System.err.println("Specify value of noise level.");
-                return;
-            }
             centroidMassDetectorParameters.getParameter(CentroidMassDetectorParameters.noiseLevel).setValue(noiseLevel);
             MZmineProcessingStep<MassDetector> centroidMassDetector =
                     new MZmineProcessingStepImpl<>(massDetectors[0], centroidMassDetectorParameters);
             parameters.getParameter(MassDetectionParameters.massDetector).setValue(centroidMassDetector);
-        } else{
+        } else {
             WaveletMassDetectorParameters waveletMassDetectorParameters = new WaveletMassDetectorParameters();
-                if(noiseLevel == null){
-                    noiseLevel = 1.0E2;
-                }
-                waveletMassDetectorParameters.getParameter(WaveletMassDetectorParameters.noiseLevel).setValue(noiseLevel);
-                waveletMassDetectorParameters.getParameter(WaveletMassDetectorParameters.scaleLevel).setValue(scaleLevel);
-                waveletMassDetectorParameters.getParameter(WaveletMassDetectorParameters.waveletWindow).setValue(windowSize);
+            waveletMassDetectorParameters.getParameter(WaveletMassDetectorParameters.noiseLevel).setValue(noiseLevel);
+            waveletMassDetectorParameters.getParameter(WaveletMassDetectorParameters.scaleLevel).setValue(scaleLevel);
+            waveletMassDetectorParameters.getParameter(WaveletMassDetectorParameters.waveletWindow).setValue(windowSize);
             MZmineProcessingStep<MassDetector> waveletMassDetector =
                     new MZmineProcessingStepImpl<>(massDetectors[4], waveletMassDetectorParameters);
             parameters.getParameter(MassDetectionParameters.massDetector).setValue(waveletMassDetector);
@@ -183,11 +195,11 @@ public class Main {
 
         parameters.getParameter(MassDetectionParameters.name).setValue("masses");
         parameters.getParameter(MassDetectionParameters.scanSelection).setValue(new ScanSelection());
-        parameters.outFilenameOption.getEmbeddedParameter().setValue(outputFile);
+        parameters.getParameter(MassDetectionParameters.outFilenameOption).getEmbeddedParameter().setValue(outputFile);
         parameters.getParameter(MassDetectionParameters.outFilenameOption).setValue(Boolean.TRUE);
 
-        MassDetectionTask massDetectionTask = new MassDetectionTask(rawDataFile,parameters);
-        massDetectionTask.run();
-
+        return parameters;
     }
+
+
 }
